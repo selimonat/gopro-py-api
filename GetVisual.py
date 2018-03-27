f77b3cb90b3d9659e6fc853c3f0b5c3af2d1093b
selimonat <onatselim@gmail.com>
2018-03-27 10:57:55 +0200

    made it a definition

--- GetVisual.py
+++ GetVisual.py
@@ -1,13 +1,32 @@
-#!/home/pi/miniconda3/bin/python3
+#!/home/pi/miniconda3/envs/py36/bin/python
 from goprocam import constants;
 from goprocam import GoProCamera;
 import time
 import subprocess
 import os
+import sys
 
-gpCam = GoProCamera.GoPro(constants.auth);
-gpCam.take_photo(1)
-time.sleep(2)
-gpCam.downloadLastMedia(custom_filename="/home/pi/getvisuals_log/")
-print(os.getcwd())
-subprocess.Popen(["/home/pi/code/shell/bin/SendLastPic.sh","/home/pi/getvisuals_log/", "onatselim@gmail.com"])
+def main():
+	sys.path.append(os.path.abspath("/home/pi/code/python/face_recognition/examples/"))
+	from find_faces import facefuck
+	
+	gpCam = GoProCamera.GoPro(constants.auth)
+	gpCam.take_photo(1)
+	time.sleep(2)
+	save_path  = "/home/pi/getvisuals_log/"
+	filename   = save_path + str(round(time.time())) + ".jpg"
+	gpCam.downloadLastMedia(custom_filename=filename)
+	
+	
+	print("resizing image to 50%")
+	subprocess.call(["/usr/bin/convert {} -resize 25% {}".format(filename,filename)],shell=True)
+	print("detecting faces in the image with facefuck")
+	facefuck(filename)
+	print("sending results as an email")
+	subprocess.Popen(["/home/pi/code/shell/bin/SendLastPic.sh","/home/pi/getvisuals_log/","'Email sent by GetVisual.py'","Visual from your home", "onatselim@gmail.com"])
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
+    
